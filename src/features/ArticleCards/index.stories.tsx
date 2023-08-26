3fe01cee0dd83fefcc46b9861e5b470d7fd8cbbd
yutteee <yusaku_nakamura_aa@icloud.com>
2023-08-26 22:07:14 +0900

    項目のPropsを実装

--- src/features/ArticleCards/index.stories.tsx
+++ src/features/ArticleCards/index.stories.tsx
@@ -1,10 +1,22 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import { ArticleCardsPresentation } from './presentations/';
+import { ArticleCardsPresentation } from "./presentations/";
 
-type T = typeof ArticleCardsPresentation
+type T = typeof ArticleCardsPresentation;
 
 export default {
   component: ArticleCardsPresentation,
 } satisfies Meta<T>;
 
-export const Default: StoryObj<T> = {};
+export const Default: StoryObj<T> = {
+  args: {
+    imageUrl: "https://picsum.photos/100/100",
+    title: "Title",
+    tags: [
+      {
+        id: "1",
+        name: "Tag1",
+        color: "#000000",
+      },
+    ],
+  },
+};
--- src/features/ArticleCards/presentations/index.tsx
+++ src/features/ArticleCards/presentations/index.tsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import React from "react";
+import Image from "next/image";
 
-type Props = {};
+type Tag = {
+  id: string;
+  name: string;
+  color: string;
+};
 
-export const ArticleCardsPresentation: React.FC<Props> = (props) => {
+type Props = {
+  imageUrl?: string;
+  title: string;
+  tags: Tag[];
+};
+
+export const ArticleCardsPresentation: React.FC<Props> = ({
+  imageUrl = "https://placehold.jp/100x100.png",
+  title,
+  tags,
+}) => {
   return (
     <div>
-      <h1>Component</h1>
+      <Image src={imageUrl} alt={title} width={100} height={100} />
+      <h3>{title}</h3>
+      <ul>
+        {tags.map((tag) => (
+          <li key={tag.id}>{tag.name}</li>
+        ))}
+      </ul>
     </div>
   );
 };
