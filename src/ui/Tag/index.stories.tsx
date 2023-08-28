c82a081b72fe29e02bd288cffbfded88fc37ef5a
yutteee <yusaku_nakamura_aa@icloud.com>
2023-08-28 18:40:09 +0900

    tagのレイアウト #7

--- src/ui/Tag/index.stories.tsx
+++ src/ui/Tag/index.stories.tsx
@@ -1,10 +1,61 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import { Tag } from './';
+import { Tag } from "./";
 
-type T = typeof Tag
+type T = typeof Tag;
 
 export default {
   component: Tag,
 } satisfies Meta<T>;
 
-export const Default: StoryObj<T> = {};
+export const Default: StoryObj<T> = {
+  args: {
+    name: "Tag",
+    color: "default",
+  },
+  argTypes: {
+    color: {
+      control: {
+        type: "select",
+        options: [
+          "blue",
+          "brown",
+          "default",
+          "gray",
+          "green",
+          "orange",
+          "pink",
+          "purple",
+          "red",
+          "yellow",
+        ],
+      },
+    },
+  },
+};
+
+export const Button: StoryObj<T> = {
+  args: {
+    name: "Tag",
+    color: "default",
+    isButton: true,
+  },
+  argTypes: {
+    color: {
+      control: {
+        type: "select",
+        options: [
+          "blue",
+          "brown",
+          "default",
+          "gray",
+          "green",
+          "orange",
+          "pink",
+          "purple",
+          "red",
+          "yellow",
+        ],
+      },
+    },
+  },
+};
--- src/ui/Tag/index.tsx
+++ src/ui/Tag/index.tsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import React from "react";
+import { Color, convertToColor } from "./logics";
 
-type Props = {};
+type Props = {
+  name: string;
+  color: Color;
+  isButton?: boolean;
+};
 
-export const Tag: React.FC<Props> = (props) => {
-  return (
-    <div>
-      <h1>Component</h1>
+export const Tag: React.FC<Props> = ({ name, color, isButton = false }) => {
+  const colorCode = convertToColor[color];
+
+  return isButton ? (
+    <button
+      className="rounded px-20 py-10 text-detail2 text-white hover:opacity-80 active:opacity-60"
+      style={{
+        backgroundColor: colorCode,
+      }}
+    >
+      {name}
+    </button>
+  ) : (
+    <div
+      className="w-fit rounded px-20 py-10 text-detail2 text-white"
+      style={{
+        backgroundColor: colorCode,
+      }}
+    >
+      {name}
     </div>
   );
 };
