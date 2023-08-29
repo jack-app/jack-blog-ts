cd3d2cc6c63397911907602501dd39665fa31983
yutteee <yusaku_nakamura_aa@icloud.com>
2023-08-29 12:18:12 +0900

    記事一覧を取得するためのhooksを作成 #10

--- src/features/ArticleCardList/hooks/index.ts
+++ src/features/ArticleCardList/hooks/index.ts
@@ -4,5 +4,23 @@ export const useGetArticles = async () => {
   const databaseId = process.env.NOTION_DATABASE_ID;
   const articles = await getDatabase(databaseId);
 
-  return articles;
+  const results = articles
+    .filter((article: any) => {
+      return article.properties.Publish.checkbox === true;
+    })
+    .map((article: any) => {
+      return {
+        id: article.id,
+        image: article.cover ? article.cover.url : null,
+        title: article.properties.Name.title[0].plain_text,
+        tags: article.properties.tag.multi_select,
+      };
+    });
+
+  return results as {
+    id: string;
+    image: string | null;
+    title: string;
+    tags: { id: string; name: string; color: string }[];
+  }[];
 };
