1fe986df360f463796a07708b250b312d509725c
yutteee <yusaku_nakamura_aa@icloud.com>
2023-08-31 12:48:02 +0900

    テストを追加 #10

--- src/features/ArticleCardList/tests/index.test.tsx
+++ src/features/ArticleCardList/tests/index.test.tsx
@@ -1,13 +1,66 @@
-import '@testing-library/jest-dom'
-import { ArticleCards } from '..'
-import { waitFor, render, screen } from '@testing-library/react'
+import "@testing-library/jest-dom";
+import { ArticleCardList } from "..";
+import { waitFor, render, screen } from "@testing-library/react";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 
-it('render ArticleCards component', () => {
-  render(<ArticleCards />)
+const setUp = (ui: React.ReactElement) => {
+  return render(
+    <Suspense fallback={<div>Loading...</div>}>
+      <ErrorBoundary fallback={<div>error</div>}>{ui}</ErrorBoundary>
+    </Suspense>
+  );
+};
 
-  const heading = screen.getByRole('heading', {
-    name: 'Component',
-  })
+jest.mock("../hooks", () => {
+  return {
+    useArticles: () => {
+      return [
+        {
+          id: "1",
+          imageUrl: "https://placehold.jp/360x200.png",
+          title: "jack-blogを支える技術",
+          tags: [
+            {
+              id: "1",
+              name: "frontend",
+              color: "default",
+            },
+            {
+              id: "2",
+              name: "Next.js",
+              color: "blue",
+            },
+          ],
+        },
+        {
+          id: "2",
+          title: "だから僕はtailwindをやめた",
+          tags: [
+            {
+              id: "1",
+              name: "frontend",
+              color: "default",
+            },
+            {
+              id: "3",
+              name: "tailwind",
+              color: "green",
+            },
+          ],
+        },
+      ];
+    },
+  };
+});
 
-  expect(heading).toBeInTheDocument()
-})
+describe("ArticleCardList", () => {
+  test("ArticleCardListに渡したitemsが表示されるか", async () => {
+    setUp(<ArticleCardList />);
+
+    await waitFor(() => {
+      expect(screen.getByRole("link", { name: "jack-blogを支える技術" })).toBeInTheDocument();
+      expect(screen.getByRole("link", { name: "だから僕はtailwindをやめた" })).toBeInTheDocument();
+    });
+  });
+});
