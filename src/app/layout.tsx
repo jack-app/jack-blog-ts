314686fe7e9e1149e4ab8abb992ddb65a96f56ee
yutteee <yusaku_nakamura_aa@icloud.com>
2023-09-04 21:15:10 +0900

    tailwindに書き換え #14

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Header } from "@/ui/Header";
+import { Footer } from "@/ui/Footer";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -13,6 +14,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body>
         <Header />
         <main>{children}</main>
+        <Footer />
       </body>
     </html>
   );
--- src/ui/Footer/index.tsx
+++ src/ui/Footer/index.tsx
@@ -1,28 +1,29 @@
 import React from "react";
 import Image from "next/image";
-import styles from "./Footer.module.css";
 import { MdOutlineWeb } from "react-icons/md";
 import { BsTwitter } from "react-icons/bs";
 import { MdEmail } from "react-icons/md";
 
 export const Footer = () => {
   return (
-    <footer className={styles.footer}>
-      <div className={styles.logo}>
-        <Image src="/logo.png" width={100} height={70} alt="logo" />
+    <footer className="w-full bg-primary-500 p-20">
+      <div className="mx-auto flex w-full max-w-screen-xl items-center justify-around">
+        <div className="flex w-6/12 justify-center">
+          <Image src="/logo.png" width={100} height={70} alt="logo" />
+        </div>
+        <div className="flex w-6/12 justify-evenly">
+          <a href="https://jack-website.netlify.app/" target="_blank" rel="noopener noreferrer">
+            <MdOutlineWeb size={40} color="#fff" />
+          </a>
+          <a href="https://twitter.com/jackapp_tmy" target="_blank" rel="noopener noreferrer">
+            <BsTwitter size={36} color="#fff" />
+          </a>
+          <a href="mailto:jack.app.tmy@gmail.com" target="_blank" rel="noopener noreferrer">
+            <MdEmail size={36} color="#fff" />
+          </a>
+        </div>
       </div>
-      <div className={styles.links}>
-        <a href="https://jack-website.netlify.app/" target="_blank" rel="noopener noreferrer">
-          <MdOutlineWeb size={40} color="#fff" />
-        </a>
-        <a href="https://twitter.com/jackapp_tmy" target="_blank" rel="noopener noreferrer">
-          <BsTwitter size={36} color="#fff" />
-        </a>
-        <a href="mailto:jack.app.tmy@gmail.com" target="_blank" rel="noopener noreferrer">
-          <MdEmail size={36} color="#fff" />
-        </a>
-      </div>
-      <div className={styles.copy}>&copy;jack 2023</div>
+      <div className="flex justify-center text-white">&copy;jack 2023</div>
     </footer>
   );
 };
