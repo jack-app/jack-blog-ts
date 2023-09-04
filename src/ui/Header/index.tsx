399ea10d1641d493e6f8b1eba92305a7bd91e849
yutteee <yusaku_nakamura_aa@icloud.com>
2023-09-04 20:50:48 +0900

    tailwindに書き換え #14

--- src/ui/Header/index.tsx
+++ src/ui/Header/index.tsx
@@ -1,15 +1,16 @@
 import React from "react";
 import Image from "next/image";
-import styles from "./index.module.css";
 
 export const Header = () => {
   return (
-    <header className={styles.header}>
-      <div className={styles.titles}>
-        <div className={styles.mainTitle}>jack blog</div>
-        <div className={styles.subTitle}>アプリ開発団体jack メンバーズブログ</div>
+    <header className="relative aspect-[4/1] w-full">
+      <div className="absolute top-1/2 z-20 translate-y-[-50%] pl-[5%]">
+        <div className="pb-60 font-['Inter'] text-[32px] font-thin tracking-widest text-white">
+          jack blog
+        </div>
+        <div className="text-[20px] text-white">アプリ開発団体jack メンバーズブログ</div>
       </div>
-      <div className={styles.overlay}></div>
+      <div className="absolute z-10 h-full w-full bg-gradient-to-r from-primary-500 to-primary-900 opacity-90" />
       <Image src="/HeaderBackground.png" alt="header" fill />
     </header>
   );
