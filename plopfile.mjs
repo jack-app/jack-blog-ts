7104e8be5c9318a13a5eb5d337aad9423063d8cf
yutteee <yusaku_nakamura_aa@icloud.com>
2023-09-06 19:06:15 +0900

    plopにfeaturesを追加 #33

--- plopfile.mjs
+++ plopfile.mjs
@@ -2,6 +2,43 @@ export default function (
   /** @type {import('plop').NodePlopAPI} */
   plop
 ) {
+  plop.setGenerator("feature", {
+    description: "Create a new feature component",
+    prompts: [
+      {
+        type: "input",
+        name: "name",
+        message: "What is the name of the feature component?",
+      },
+    ],
+    actions: [
+      {
+        type: "add",
+        path: "src/features/{{pascalCase name}}/index.tsx",
+        templateFile: "plop-templates/feature/feature.tsx.hbs",
+      },
+      {
+        type: "add",
+        path: "src/features/{{pascalCase name}}/index.stories.tsx",
+        templateFile: "plop-templates/feature/feature.stories.tsx.hbs",
+      },
+      {
+        type: "add",
+        path: "src/features/{{pascalCase name}}/tests/index.test.tsx",
+        templateFile: "plop-templates/feature/tests/feature.test.tsx.hbs",
+      },
+      {
+        type: "add",
+        path: "src/features/{{pascalCase name}}/presentations/index.tsx",
+        templateFile: "plop-templates/feature/presentations/feature.tsx.hbs",
+      },
+      {
+        type: "add",
+        path: "src/features/{{pascalCase name}}/hooks/index.ts",
+        templateFile: "plop-templates/feature/hooks/feature.ts.hbs",
+      },
+    ],
+  });
   plop.setGenerator("components", {
     description: "Create a new component",
     prompts: [
