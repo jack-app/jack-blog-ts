39721958d826017b2b2b9c17c250cf3baf92ae7c
yutteee <yusaku_nakamura_aa@icloud.com>
2023-09-21 14:14:15 +0900

    renderでの不要なcssを削除

--- src/features/ArticleDetailBody/hooks/renderNestedList.tsx
+++ src/features/ArticleDetailBody/hooks/renderNestedList.tsx
@@ -1,10 +1,9 @@
+import { Block } from "@/types/block";
 import { renderBlock } from "./renderBlock";
 import { BulletedListPresentation } from "../presentations/blocks/bulletedList";
 import { NumberedListPresentation } from "../presentations/blocks/numberedList";
 
-export const renderNestedList = (block: any, pageId: string) => {
-  console.log(block);
-
+export const renderNestedList = (block: Block, pageId: string) => {
   const isNumberedList = block.type === "numbered_list_item";
 
   if (isNumberedList) {
