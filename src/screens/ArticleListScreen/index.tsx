b466122db2d4584db915236ff73702202f0b09d6
yutteee <yusaku_nakamura_aa@icloud.com>
2023-09-23 02:11:30 +0900

    タグとwriterをスクロールしても表示 #62

--- src/screens/ArticleListScreen/index.tsx
+++ src/screens/ArticleListScreen/index.tsx
@@ -9,9 +9,9 @@ type Props = {
 
 export const ArticleListScreen: React.FC<Props> = async ({ tag, writer }) => {
   return (
-    <main className="my-70  flex justify-center gap-90 md:my-90">
+    <main className="my-70 flex justify-center gap-90 md:my-90">
       <ArticleList tag={tag} writer={writer} />
-      <div className="hidden flex-col gap-60 lg:flex">
+      <div className="sticky top-[16px] hidden h-fit flex-col gap-60 lg:flex">
         <SearchTags />
         <SearchWriters />
       </div>
