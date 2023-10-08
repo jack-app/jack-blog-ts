eea9dc15f945f8900d106b8f5c7427051c932ea7
yutteee <yusaku_nakamura_aa@icloud.com>
2023-10-08 23:58:50 +0900

    fontの設定 #38

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -11,6 +11,8 @@ export async function generateMetadata({ params }: { params: { id: string } }): 
   const writerName = page.properties.Writer.created_by.name;
   const description = "アプリ開発団体jackのブログサイトです。";
 
+  const fontData = (await getFontData()) as ArrayBuffer;
+
   // ogp画像を動的に生成。
   // Next.jsのデフォルトでサポートされているが、edgeランタイムで使用するとエラーになるため、satoriを使用。
   const svg = await satori(
@@ -46,7 +48,13 @@ export async function generateMetadata({ params }: { params: { id: string } }): 
     {
       width: 1200,
       height: 600,
-      fonts: [],
+      fonts: [
+        {
+          name: "Noto Sans JP",
+          data: fontData,
+          style: "normal",
+        },
+      ],
     }
   );
 
@@ -68,6 +76,24 @@ export async function generateMetadata({ params }: { params: { id: string } }): 
       images: [svg],
     },
   };
+}
+
+// fontの設定、参考：https://blog.70-10.net/posts/satori-og-image/
+async function getFontData() {
+  const API = `https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@700`;
+
+  const css = await (
+    await fetch(API, {
+      headers: {
+        "User-Agent":
+          "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_8; de-at) AppleWebKit/533.21.1 (KHTML, like Gecko) Version/5.0.5 Safari/533.21.1",
+      },
+    })
+  ).text();
+  const resource = css.match(/src: url\((.+)\) format\('(opentype|truetype)'\)/);
+  if (!resource) return;
+
+  return await fetch(resource[1]).then((res) => res.arrayBuffer());
 }
 
 export async function generateStaticParams() {
