ccb469068ae987cb04996d95d04c3f88c1b212a8
yutteee <yusaku_nakamura_aa@icloud.com>
2023-10-08 21:23:58 +0900

    satoriを用いてogpを動的に生成するようにした #38

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,58 +1,54 @@
 import { Metadata } from "next";
+import satori from "satori";
 import { ArticleDetailScreen } from "@/screens/ArticleDetailScreen";
 import { getDatabase } from "@/utils/notion";
 import { getPage } from "@/utils/notion";
-
-/**
- * OGP画像を生成するための処理
- * https://nextjs.org/docs/app/building-your-application/optimizing/metadata#dynamic-image-generation
- * edgeのランタイムで実行する必要があるが、fsが使えないエラーがでる。要調査。
- */
-// export const runtime = "edge";
-// export async function GET({ params }: { params: { id: string } }) {
-//   const page: any = await getPage(params.id);
-
-//   const title = page.properties.Name.title[0].plain_text;
-//   const writerName = page.properties.Writer.created_by.name;
-
-//   return new ImageResponse(
-//     (
-//       <div
-//         style={{
-//           height: "100%",
-//           width: "100%",
-//           display: "flex",
-//           flexDirection: "column",
-//           alignItems: "center",
-//           justifyContent: "center",
-//           gap: "16px",
-//           backgroundImage: "linear-gradient(90deg, #FFC121, #FF5E2C)",
-//           color: "white",
-//           fontFamily: '"Avenir", "hiragino kaku gothic pro", "sans-serif"',
-//         }}
-//       >
-//         <div style={{ fontSize: "32px" }}>{title}</div>
-//         <div style={{ fontSize: "20px" }}>@{writerName}</div>
-//       </div>
-//     ),
-//     {
-//       width: 1200,
-//       height: 600,
-//     }
-//   );
-// }
 
 export async function generateMetadata({ params }: { params: { id: string } }): Promise<Metadata> {
   const page: any = await getPage(params.id);
 
   const title = page.properties.Name.title[0].plain_text;
+  const writerName = page.properties.Writer.created_by.name;
   const description = "アプリ開発団体jackのブログサイトです。";
-  const image = {
-    url: "/ogp.png",
-    width: 1200,
-    height: 600,
-    alt: "jack blog",
-  };
+
+  // ogp画像を動的に生成。
+  // Next.jsのデフォルトでサポートされているが、edgeランタイムで使用するとエラーになるため、satoriを使用。
+  const svg = await satori(
+    <div
+      style={{
+        height: "100%",
+        width: "100%",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundImage: "linear-gradient(90deg, #FFC121, #FF5E2C)",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          borderRadius: "8px",
+          gap: "16px",
+          backgroundColor: "#FFFCF2",
+          color: "#484335",
+          width: "90%",
+          height: "85%",
+          padding: "32px",
+        }}
+      >
+        <div style={{ fontSize: "32px" }}>{title}</div>
+        <div style={{ fontSize: "20px" }}>@{writerName}</div>
+      </div>
+    </div>,
+    {
+      width: 1200,
+      height: 600,
+      fonts: [],
+    }
+  );
 
   return {
     title,
@@ -63,13 +59,13 @@ export async function generateMetadata({ params }: { params: { id: string } }): 
       siteName: title,
       title: title,
       description: description,
-      images: [image],
+      images: [svg],
     },
     twitter: {
       card: "summary_large_image",
       title: title,
       description: description,
-      images: [image],
+      images: [svg],
     },
   };
 }
