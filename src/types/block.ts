51a5664b5e9ea037dc082c0f1bef50bffe2200a2
yutteee <yusaku_nakamura_aa@icloud.com>
2023-12-31 12:26:47 +0900

    typeを定義 #55

--- src/types/block.ts
+++ src/types/block.ts
@@ -1,12 +1,225 @@
-// TODO: notion apiから取ってこれる全てのblock要素をここで定義したい。
 // 参考：https://developers.notion.com/reference/block
 
-export type Block = any;
+export type Database = {
+  title: string;
+  description: string;
+  icon: FileObject | Emoji | null;
+  cover: FileObject | null;
+};
+
+export type Post = {
+  page_id: string;
+  title: string;
+  icon: FileObject | Emoji | null;
+  cover: FileObject | null;
+  slug: string;
+  date: string;
+  tags: SelectProperty[];
+  excerpt: string;
+  featured_image: FileObject | null;
+  rank: number;
+};
+
+export type Block = {
+  id: string;
+  type: string;
+  has_children: boolean;
+  paragraph?: Paragraph;
+  heading1?: Heading1;
+  heading2?: Heading2;
+  heading3?: Heading3;
+  bulleted_list_item?: BulletedListItem;
+  numbered_list_item?: NumberedListItem;
+  toDo?: ToDo;
+  image?: Image;
+  file?: File;
+  code?: Code;
+  quote?: Quote;
+  equation?: Equation;
+  callout?: Callout;
+  synced_block?: SyncedBlock;
+  toggle?: Toggle;
+  embed?: Embed;
+  video?: Video;
+  bookmark?: Bookmark;
+  link_preview?: LinkPreview;
+  table?: Table;
+  column_list?: ColumnList;
+  table_of_contents?: TableOfContents;
+  link_to_page?: LinkToPage;
+  children?: Block[];
+};
+
+export type Paragraph = {
+  rich_text: RichText[];
+  color: string;
+  children?: Block[];
+};
+
+export type Heading1 = {
+  rich_text: RichText[];
+  color: string;
+  is_toggleable: boolean;
+  children?: Block[];
+};
+
+export type Heading2 = {
+  rich_text: RichText[];
+  color: string;
+  is_toggleable: boolean;
+  children?: Block[];
+};
+
+export type Heading3 = {
+  rich_text: RichText[];
+  color: string;
+  is_toggleable: boolean;
+  children?: Block[];
+};
+
+export type BulletedListItem = {
+  rich_text: RichText[];
+  color: string;
+  children?: Block[];
+};
+
+export type NumberedListItem = {
+  rich_text: RichText[];
+  color: string;
+  children?: Block[];
+};
+
+export type ToDo = {
+  rich_text: RichText[];
+  checked: boolean;
+  color: string;
+  children?: Block[];
+};
+
+export type Image = {
+  caption: RichText[];
+  type: string;
+  file?: FileObject;
+  external?: External;
+  width?: number;
+  height?: number;
+};
+
+export type Video = {
+  caption: RichText[];
+  type: string;
+  external?: External;
+};
+
+export type File = {
+  caption: RichText[];
+  type: string;
+  file?: FileObject;
+  external?: External;
+};
+
+export type FileObject = {
+  type: string;
+  url: string;
+  expiry_time?: string;
+};
+
+export type External = {
+  url: string;
+};
+
+export type Code = {
+  caption: RichText[];
+  rich_text: RichText[];
+  language: string;
+};
+
+export type Quote = {
+  rich_text: RichText[];
+  color: string;
+  children?: Block[];
+};
+
+export type Equation = {
+  expression: string;
+};
+
+export type Callout = {
+  rich_text: RichText[];
+  icon: FileObject | Emoji | null;
+  color: string;
+  children?: Block[];
+};
+
+export type SyncedBlock = {
+  synced_from: SyncedFrom | null;
+  children?: Block[];
+};
+
+export type SyncedFrom = {
+  blockId: string;
+};
+
+export type Toggle = {
+  rich_text: RichText[];
+  color: string;
+  children: Block[];
+};
+
+export type Embed = {
+  url: string;
+};
+
+export type Bookmark = {
+  url: string;
+};
+
+export type LinkPreview = {
+  url: string;
+};
+
+export type Table = {
+  table_width: number;
+  has_column_header: boolean;
+  has_row_header: boolean;
+  rows: TableRow[];
+};
+
+export type TableRow = {
+  id: string;
+  type: string;
+  has_children: boolean;
+  cells: TableCell[];
+};
+
+export type TableCell = {
+  rich_text: RichText[];
+};
+
+export type ColumnList = {
+  columns: Column[];
+};
+
+export type Column = {
+  id: string;
+  type: string;
+  has_children: boolean;
+  children: Block[];
+};
+
+export type List = {
+  type: string;
+  listItems: Block[];
+};
+
+export type TableOfContents = {
+  color: string;
+};
 
 export type RichText = {
   text?: Text;
   annotations: Annotations;
-  plainText: string;
+  plain_text: string;
   href?: string;
   equation?: Equation;
   mention?: Mention;
@@ -17,8 +230,9 @@ export type Text = {
   link?: Link;
 };
 
-export type Link = {
-  url: string;
+export type Emoji = {
+  type: string;
+  emoji: string;
 };
 
 export type Annotations = {
@@ -27,32 +241,22 @@ export type Annotations = {
   strikethrough: boolean;
   underline: boolean;
   code: boolean;
-  color: Color;
+  color: string;
 };
 
-export type Color =
-  | "blue"
-  | "blue_background"
-  | "brown"
-  | "brown_background"
-  | "default"
-  | "gray"
-  | "gray_background"
-  | "green"
-  | "green_background"
-  | "orange"
-  | "orange_background"
-  | "pink"
-  | "pink_background"
-  | "purple"
-  | "purple_background"
-  | "red"
-  | "red_background"
-  | "yellow"
-  | "yellow_background";
+export type Link = {
+  url: string;
+};
 
-export type Equation = {
-  expression: string;
+export type SelectProperty = {
+  id: string;
+  name: string;
+  color: string;
+};
+
+export type LinkToPage = {
+  type: string;
+  page_id: string;
 };
 
 export type Mention = {
@@ -62,10 +266,4 @@ export type Mention = {
 
 export type Reference = {
   id: string;
-};
-
-export type Code = {
-  Caption: RichText[];
-  RichTexts: RichText[];
-  Language: string;
 };
