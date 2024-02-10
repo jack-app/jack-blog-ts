2034768d15bdac3fe4cf038dbd1be9542f6a7e2b
yutteee <yusaku_nakamura_aa@icloud.com>
2024-02-10 14:56:19 +0900

    headerのデザイン修正 #93

--- src/ui/Header/index.tsx
+++ src/ui/Header/index.tsx
@@ -3,23 +3,27 @@ import React from "react";
 
 export const Header = () => {
   return (
-    <header className="relative aspect-[4/1] h-[48px] max-h-[400px] w-full sm:h-auto">
+    <header className="relative aspect-[4/1] h-[48px] max-h-[400px] w-full lg:h-auto">
       <div className="absolute top-1/2 z-20 translate-y-[-50%] pl-[5%]">
         <Link
           href="/"
-          className="font-['Inter'] text-[16px] font-thin text-white sm:text-[24px] sm:tracking-widest md:text-[32px]"
+          className="font-['Inter'] text-[16px] font-thin text-white lg:text-[32px] lg:tracking-widest"
         >
           jack blog
         </Link>
-        <div className="hidden pt-50 text-[16px] text-white sm:block md:pt-60 md:text-[20px]">
+        <div className="hidden pt-50 text-[16px] text-white md:pt-60 lg:block lg:text-[20px]">
           アプリ開発団体jack メンバーズブログ
         </div>
       </div>
-      <div className="absolute z-10 h-full max-h-[400px] w-full bg-gradient-to-r from-primary to-secondary opacity-90" />
+      <div className="absolute right-0 top-1/4 z-20 pr-[5%] text-white lg:hidden">
+        <Link href="/writer">作者一覧</Link>
+      </div>
+
+      <div className="absolute z-10 h-full max-h-[400px] w-full bg-[#FFA61F] lg:bg-gradient-to-r lg:from-primary lg:to-secondary lg:opacity-90" />
       <img
         src="/HeaderBackground.png"
         alt="header"
-        className="hidden aspect-[4/1] h-[48px] max-h-[400px] w-full sm:block sm:h-auto sm:object-cover"
+        className="hidden aspect-[4/1] h-[48px] max-h-[400px] w-full lg:block lg:h-auto lg:object-cover"
       />
     </header>
   );
